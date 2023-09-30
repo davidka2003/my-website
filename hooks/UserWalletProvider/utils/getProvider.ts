3a3856f9154e522967ab68684e0cfcc6f44ac93c
davidka2003 <d4v1ds0n.p@gmail.com>
2023-09-30 21:21:41 +0300

    updae

--- hooks/UserWalletProvider/utils/getProvider.ts
+++
@@ -1,6 +0,0 @@
-import { ethers } from "ethers";
-
-export const getProvider = (instance: any) => {
-  const provider = new ethers.providers.Web3Provider(instance);
-  return provider;
-};
--- hooks/UserWalletProvider/utils/getSigner.ts
+++
@@ -1,4 +0,0 @@
-import { ethers } from "ethers";
-
-export const getSigner = (provider: ethers.providers.Web3Provider) =>
-  provider.getSigner();
